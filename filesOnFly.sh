bdfa2aac265de0fe7a6ac1ddcba9d986a4747753
Денис Егоров <37404390+NightBlade97@users.noreply.github.com>
2021-04-23 01:30:33 +0300

    Update filesOnFly.sh

--- filesOnFly.sh
+++ filesOnFly.sh
@@ -1,8 +1,11 @@
-!/bin/bash
-FILES=../../ilesForWordcount/*
+#!/bin/bash
+FILES=../../filesForWordcount/*
 for f in $FILES
 do
-       	kafka-console-producer --broker-list localhost:9092 --topic wordCount-topic < "$f"
-        rm "$f"
+		if test -f "$f"; then
+			bin/kafka-console-producer.sh --broker-list localhost:9092 --topic wordCount-topic < "$f"
+			rm "$f"
+		fi
+
 done
 
